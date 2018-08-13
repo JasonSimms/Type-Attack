36b3a5ca98593e10726a6bcdd794d7290c756396
GitGut0110 <jsimms0110@gmail.com>
2018-08-13 17:50:27 +0200

    Game broken when trying to include many modes.Introduce one at a time of collecting string from mode file.

--- components.js
+++ components.js
@@ -1,41 +1,44 @@
-console.log('components loaded')
+console.log("components loaded");
 
-function Component(width, height, color, x, y, speed,char) {
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    this.char = char;
-    // ctx = myGameArea.context;
-    this.update = function(){
-        ctx.fillStyle = 'white'
-        ctx.fillRect(this.x -3, this.y- 27, this.width, this.height);
-        ctx.fillStyle = 'black';
-        ctx.font="50px Georgia"
-        ctx.fillText(this.char,this.x,this.y,this.width,this.height)
-    }
+function Component(width, height, color, x, y, speed, char) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  this.char = char;
+  // ctx = myGameArea.context;
+  this.update = function() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(this.x - 3, this.y - 27, this.width, this.height);
+    ctx.fillStyle = "black";
+    ctx.font = "50px Georgia";
+    ctx.fillText(this.char, this.x, this.y, this.width, this.height);
+  };
 
-
-    this.newPos = function() {
-        this.x -= this.speed;
-        // this.y += this.speedY; 
-        // console.log('newPOs',this.x)
-}
+  this.newPos = function() {
+    this.x -= this.speed;
+    // this.y += this.speedY;
+    // console.log('newPOs',this.x)
+  };
 }
 
 function stop() {
-    console.log('game terminated')
-    isGameStarted = false;
-    clearInterval(interval);
-    ctx.font = "40px bold";
-    ctx.fillStyle = "red";
-    if (myObstacles.length === 0){
+  console.log("game terminated");
+  isGameStarted = false;
+  clearInterval(interval);
+  ctx.font = "40px bold";
+  ctx.fillStyle = "red";
+  if (myObstacles.length === 0) {
     ctx.fillText("Shiny!", 400, 400);
-    ctx.fillText(("Score: "+score), 400, 100);
-
-    }else{
-    ctx.fillText("Gameover!", 400, 400);
-    ctx.fillText(("Score: "+score), 400, 100)};
-    player.speedY = 0;
+  } else {
+    ctx.fillText("Gameover! There is no try", 400, 400);
+}
+ctx.fillText("Score: " + score, 400, 100);
+  console.log("weiter");
+  if (score > topScore) {
+    topScore = score;
   }
+  $('.top-score').html('Your Top Score: '+topScore)
+  console.log('topScore:',topScore)
+}
---
+++ gamemodes.js
@@ -0,0 +1,8 @@
+console.log('strings loaded')
+// Here are input strings for gameplay various modes
+// Every letter at Random
+word = 'abcdefghijklmnopqrstuvwxyz'
+var strAlph = word.split('').sort(function(a,b){return 0.5-Math.random()});
+// Debug mode
+var strDebug = ['b','c','d','e']
+// Fast mode
