2f79767a9f73a3063a59dc7d16e6080a44b3d2ed
gitgut0110 <jsimms0110@gmail.com>
2018-08-15 13:00:22 +0200

    sounds not loading on github

--- JS/main.js
+++ JS/main.js
@@ -31,8 +31,8 @@ var marathonMode = false;
 // SOUNDS
 hit = new sound("sounds/lazer.mp3");
 start = new sound("sounds/laser2.mp3")
-end = new sound("../sounds/gameover.mp3");
-pop = new sound("../sounds/pop.mp3");
+end = new sound("sounds/gameover.mp3");
+pop = new sound("sounds/pop.mp3");
 // GAME Should take a string input, if the player presses the correct key
 // the string is shifted. objects will continue to fall until key is pressed.
 //  when y = canvas height GAME OVER.
