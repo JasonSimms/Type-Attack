6838eff4934cdd9ec8c5b8e7ba4d1e20895dc98c
gitgut0110 <jsimms0110@gmail.com>
2018-08-15 12:59:06 +0200

    sounds not loading on github

--- JS/main.js
+++ JS/main.js
@@ -29,8 +29,8 @@ var boost = 0;
 var spawnBoost = 0;
 var marathonMode = false;
 // SOUNDS
-hit = new sound("../sounds/lazer.mp3");
-start = new sound("../sounds/laser2.mp3")
+hit = new sound("sounds/lazer.mp3");
+start = new sound("sounds/laser2.mp3")
 end = new sound("../sounds/gameover.mp3");
 pop = new sound("../sounds/pop.mp3");
 // GAME Should take a string input, if the player presses the correct key
